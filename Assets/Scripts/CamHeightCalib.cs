c630ec9bca1e9262e56cadebc0a497c37af0e50a
Thiago Bastos <tfb@cin.ufpe.br>
2017-07-10 17:48:57 -0300

    cena para apresentação, integração com Leap

---
+++ Assets/Scripts/CamHeightCalib.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamHeightCalib : MonoBehaviour {
+
+	Transform trans;
+
+	// Use this for initialization
+	void Start () {
+		trans = GetComponent<Transform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		trans.position = new Vector3(trans.position.x, trans.position.y + Input.GetAxisRaw ("Adjust") / 100 + Input.GetAxisRaw ("Narrow Adjust") / 1000, trans.position.z);
+	}
+}
