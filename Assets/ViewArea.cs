75b956d50953c2a6ed503e3605e1987b8b3d621a
Antonio Harger Rivero <antonio.harger@gmail.com>
2017-07-10 20:26:42 -0300

    Added View Prototype

---
+++ Assets/ViewArea.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewArea : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var viewObj = other.GetComponent<ViewObject>();
+        if (viewObj != null)
+            viewObj.OnEnterView();
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        var viewObj = other.GetComponent<ViewObject>();
+        if (viewObj != null)
+            viewObj.OnExitView();
+    }
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireCube(transform.position, transform.lossyScale);
+    }
+}
---
+++ Assets/ViewObject.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class ViewObject : MonoBehaviour {
+    
+    void Awake()
+    {
+        OnExitView();
+    }
+
+    public void OnExitView()
+    {
+        foreach (var renderer in GetComponentsInChildren<Renderer>())
+        {
+            renderer.enabled = false;
+        }
+    }
+
+    public void OnEnterView()
+    {
+        foreach (var renderer in GetComponentsInChildren<Renderer>())
+        {
+            renderer.enabled = true;
+        }
+    }
+}
